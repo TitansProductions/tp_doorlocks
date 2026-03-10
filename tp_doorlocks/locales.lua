b206105c59465ecd03fee0f4621a66f516326606
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2026-03-11 00:23:57 +0200

    Create locales.lua

---
+++ tp_doorlocks/locales.lua
@@ -0,0 +1,9 @@
+Locales = {}
+
+Locales = {
+    ['LOCKED']   = "Locked",
+    ['UNLOCKED'] = "Unlocked",
+
+    ['LOCKPICKING_FAILED'] = "~e~The lockpick broke, progress has failed.",
+    ['PROPERTY_ROBBERY_BLIP_TITLE'] = "Property Robbery",
+}
